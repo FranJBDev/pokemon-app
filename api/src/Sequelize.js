9909c8163bf6364651c1ce3c9f049662909e7eda
gandhyman <gandhycoder@gmail.com>
2022-03-31 04:54:58 -0700

    Update config vars to deploy

--- api/src/Sequelize.js
+++ api/src/Sequelize.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const {
-    DB_USER, DB_PASSWORD, DB_HOST,
+    DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     freezeTableName: true //prevent sequelize from pluralizing table names
