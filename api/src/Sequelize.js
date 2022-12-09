d034acfbbf5c795eada3fefdcf6fe132f862b8bd
gandhyman <gandhycoder@gmail.com>
2022-12-09 04:26:45 -0700

    fix: sequelize var

--- api/src/Sequelize.js
+++ api/src/Sequelize.js
@@ -8,38 +8,39 @@ const DATABASE_URL = process.env;
 
 console.log('database', DATABASE_URL);
 
+// old version, only local
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
 //     logging: false, // set to console.log to see the raw SQL queries
 //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 //     freezeTableName: true //prevent sequelize from pluralizing table names
 // });
 
-// const sequelize =
-//   process.env.NODE_ENV === 'production'
-//     ? new Sequelize({
-//         database: DB_NAME,
-//         dialect: 'postgres',
-//         host: DB_HOST,
-//         port: 5432,
-//         username: DB_USER,
-//         password: DB_PASSWORD,
-//         pool: {
-//           max: 3,
-//           min: 1,
-//           idle: 10000,
-//         },
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//           },
-//           keepAlive: true,
-//         },
-//         ssl: true,
-//       })
-//     : new Sequelize(
-//         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//         { logging: false, native: false }
-//       );
+const sequelize =
+  process.env.NODE_ENV === 'production'
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: 'postgres',
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
 
 module.exports = sequelize;
