28dfdf7ec332eb967982ee586b4b2bbf0d01b640
gandhyman <gandhycoder@gmail.com>
2022-12-09 04:23:45 -0700

    Update: backend railway deploy

--- api/src/Sequelize.js
+++ api/src/Sequelize.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const {
-    DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
-} = process.env;
+// const {
+//     DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+// } = process.env;
+
+const DATABASE_URL = process.env;
+
+console.log('database', DATABASE_URL);
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
 //     logging: false, // set to console.log to see the raw SQL queries
@@ -10,33 +14,32 @@ const {
 //     freezeTableName: true //prevent sequelize from pluralizing table names
 // });
 
-const sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-      database: DB_NAME,
-      dialect: "postgres",
-      host: DB_HOST,
-      port: 5432,
-      username: DB_USER,
-      password: DB_PASSWORD,
-      pool: {
-        max: 3,
-        min: 1,
-        idle: 10000,
-      },
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-        keepAlive: true,
-      },
-      ssl: true,
-    })
-    : new Sequelize(
-      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-      { logging: false, native: false }
-    );
+// const sequelize =
+//   process.env.NODE_ENV === 'production'
+//     ? new Sequelize({
+//         database: DB_NAME,
+//         dialect: 'postgres',
+//         host: DB_HOST,
+//         port: 5432,
+//         username: DB_USER,
+//         password: DB_PASSWORD,
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//         { logging: false, native: false }
+//       );
 
-
-module.exports = sequelize
+module.exports = sequelize;
