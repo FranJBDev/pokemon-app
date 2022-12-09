b32fefd168e812198d42cf36d6bd98fd9a8c3b04
gandhyman <gandhycoder@gmail.com>
2022-12-09 04:29:22 -0700

    fix: DB_USER, DB_PASSWORD, DB_HOST, DB_NAME variables

--- api/src/Sequelize.js
+++ api/src/Sequelize.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-// const {
-//     DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
-// } = process.env;
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const DATABASE_URL = process.env;
 
